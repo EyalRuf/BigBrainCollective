f57b002878aeb049eb094175c212446c7df44650
Eyal Rufeisen <eyalrocks@gmail.com>
2020-05-18 18:46:40 +0200

    Fixed animation sync + Added synced emotes

---
+++ Assets/Scripts/Game/EmojiController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using UnityEngine;
+using Photon.Pun;
+
+namespace EyalPhoton.Game
+{
+    public class EmojiController : MonoBehaviourPun, IPunObservable
+    {
+        private const string ANIMATOR_BOOLEAN_SHOW_EMOJI = "ShowEmoji";
+        private const float EMOJI_SHOW_TIME = 2f;
+        private const float EMOJI_CD_TIME = 3.5f;
+
+        [SerializeField] private Animator animator = null;
+        [SerializeField] private PlayerInput pInput= null;
+        [SerializeField] private SpriteRenderer SR = null;
+        [SerializeField] private Sprite[] emojis = null;
+
+        private float currCdTimer = 0f;
+        private int localPlayerEmoji = 0;
+        private int remotePlayerEmoji = 0;
+        private bool localIsEmoting = false;
+
+        void Start()
+        {
+            if (!photonView.IsMine)
+            {
+                this.SR.sprite = null;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                this.currCdTimer -= Time.deltaTime;
+                if (this.currCdTimer > 0f)
+                    return;
+                else
+                    this.currCdTimer = 0f;
+
+                for (var i = 0; i < this.emojis.Length; i++)
+                {
+                    if (i < 9 && this.pInput.isNumberClicked[i + 1])
+                    {
+                        this.localPlayerEmoji = i;
+                        this.StartCoroutine(playEmoji(this.emojis[i]));
+                        break;
+                    }
+                }
+            }
+        }
+
+        private IEnumerator playEmoji (Sprite sprite)
+        {
+            this.SR.sprite = sprite;
+            this.currCdTimer = EMOJI_CD_TIME;
+            this.localIsEmoting = true;
+
+            this.animator.SetBool(ANIMATOR_BOOLEAN_SHOW_EMOJI, true);
+
+            yield return new WaitForSeconds(EMOJI_SHOW_TIME);
+
+            this.animator.SetBool(ANIMATOR_BOOLEAN_SHOW_EMOJI, false);
+            this.localIsEmoting = false;
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(this.localIsEmoting);
+                stream.SendNext(this.localPlayerEmoji);
+            }
+            else
+            {
+                bool remoteIsEmoting = (bool) stream.ReceiveNext();
+                if (remoteIsEmoting)
+                {
+                    int nextEmoji = (int) stream.ReceiveNext();
+
+                    if (this.remotePlayerEmoji != nextEmoji)
+                    {
+                        this.remotePlayerEmoji = nextEmoji;
+                        this.SR.sprite = this.emojis[nextEmoji];
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/PlayerInput.cs
+++ Assets/Scripts/Game/PlayerInput.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 namespace EyalPhoton.Game
 {
     public class PlayerInput : MonoBehaviour
     {
-        public Vector2 movementInput { get; private set; }
         [SerializeField] public bool isTest = false;
+        public Vector2 movementInput { get; private set; }
+        public bool[] isNumberClicked { get; private set; }
+
+        void Start()
+        {
+            isNumberClicked = new bool[10];
+            for (var i = 0; i < 10; i++)
+            {
+                isNumberClicked[i] = false;
+            }
+        }
 
         // Update is called once per frame
         void Update()
         {
             this.movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            this.SetNumbersClicked();
+        }
+
+        private void SetNumbersClicked()
+        {
+            this.isNumberClicked[0] = Input.GetKey(KeyCode.Alpha0);
+            this.isNumberClicked[1] = Input.GetKey(KeyCode.Alpha1);
+            this.isNumberClicked[2] = Input.GetKey(KeyCode.Alpha2);
+            this.isNumberClicked[3] = Input.GetKey(KeyCode.Alpha3);
+            this.isNumberClicked[4] = Input.GetKey(KeyCode.Alpha4);
+            this.isNumberClicked[5] = Input.GetKey(KeyCode.Alpha5);
+            this.isNumberClicked[6] = Input.GetKey(KeyCode.Alpha6);
+            this.isNumberClicked[7] = Input.GetKey(KeyCode.Alpha7);
+            this.isNumberClicked[8] = Input.GetKey(KeyCode.Alpha8);
+            this.isNumberClicked[9] = Input.GetKey(KeyCode.Alpha9);
         }
     }
 }
